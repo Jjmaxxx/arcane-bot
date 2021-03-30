4befa8887ff52a4e0f452e4be1667eff8b62954d
Jjmaxxx <justlee888@gmail.com>
2021-03-30 01:39:05 -0700

    updated help command

--- commands/help.js
+++ commands/help.js
@@ -2,19 +2,20 @@ let index = require('../index');
 let commands = [];
 const Discord = require("discord.js");
 module.exports = {
-	name: 'help',
-	description: 'show commands',
+    name: 'help',
+	description: 'shows commands',
 	execute(msg, args) {
         index.listOfCommands.map((currElement, index)=>{
-            commands += [index, currElement.description];
+            commands.push({name:index, desc: currElement.description});
         })
         let commandsList = new Discord.MessageEmbed()
         .setTitle("Here's my List of Commands")
         .setColor('0x2471a3')
         .addFields(
-            { name: commands[0][0], value: commands[0][1] },
-            { name: commands[1][0], value: commands[1][1] }
+            { name: "$" + commands[0].name, value: commands[0].desc },
+            { name: "$" +  commands[1].name, value: commands[1].desc }
         ).setFooter("What do you want from me I started using this coding library like 4 hours ago shut up");
+        msg.channel.send(commandsList);
         // console.log(index.listOfCommands.name);
         // for(let i=0; i<index.listOfCommands.length; i++){
         //     msg.reply(index.listOfCommands[i].name);
@@ -24,4 +25,4 @@ module.exports = {
         //     msg.reply((`${msg.author.tag} in #${msg.channel.name} sent: ${msg.content}`));
         // }
 	},
-};
+}
