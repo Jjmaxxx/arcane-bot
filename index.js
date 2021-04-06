264c38dfe9a116cc80c89b64fdbeeb660e307d71
Jjmaxxx <justlee888@gmail.com>
2021-04-06 11:06:12 -0700

    now pulls insult from database(need to figure out how to host database on heroku or something)

--- index.js
+++ index.js
@@ -7,25 +7,41 @@ const fs = require('fs');
 const token = config.token;
 const aws = require('aws-sdk');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-let listOfInsults = ["shut up you useless subhuman filth", "everytime you say somebody leveled up i become one step closer to a hate crime that includes you",
-"i'm going to level with you, nobody cares", "i'm going to level your ass to kingdom come if you dont shut the hell up", "shut shut shut shut shut shut shut",
-"stop it stop it stop it among us among us among us st", "i hope you step on legos", "die so you can finally provide something useful to society"]
+
 let activities = ["plotting how to effectively kill Arcane", "wondering where Arcane lives", "how do i kill a virtual bot?", "getting my alt f4 out in case i see arcane",
 "is murder illegal in this state? does it apply to virtual bots?", "every second arcane is alive another piece of my soul breaks off. and i dont even have a soul"]
 bot.login(token);
-console.log(bot.user);
+//console.log(bot.user);
 let listOfEmotes = [];
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
 	bot.commands.set(command.name, command);
 }
 const listOfCommands = bot.commands;
+
+const mongo = require("mongodb");
+let mongoClient = mongo.MongoClient;
+let url = "mongodb://localhost:27017/arcane-bot";
+let db;
+mongoClient.connect(url,{useUnifiedTopology:true},(err,client)=>{
+    db = client.db("arcane-bot");
+    //console.log(db0);
+    if(err){
+        throw err;
+    }
+    // db0.collection('insults').findOne({insult: "shut up you useless subhuman filth"},(err,result)=>{
+    //     if(err){
+    //         throw err;
+    //     }
+    //     console.log(result._id);
+    // });
+})
 bot.on('ready',()=>{
     console.log("bot ready");
     bot.user.setAvatar('./arcanepic.jpg');
     bot.user.setActivity(activities[4]);
     setInterval(() => {
-        let index = Math.floor(Math.random() * (activities.length - 1)); 
+        let index = Math.floor(Math.random() * (activities.length)); 
         bot.user.setActivity(activities[index]); 
     }, 60000); 
 })
@@ -48,19 +64,31 @@ bot.on('guildCreate', guild => {
       }
     });
   });
+  let interval;
+  let sendInsult;
+  function waitForInsult(message){
+      if(sendInsult != null){
+          message.reply(sendInsult);
+          clearInterval(interval);
+      }
+  }
+  //
 bot.on('message',(message)=>{
     //message.reply(message.author.tag);
     if(message.author.tag == "Arcane#7800"){
-        message.reply(listOfInsults[Math.round(Math.random()* (listOfInsults.length))]);
+        db.collection('insults').aggregate([{$sample: {size: 1}}]).toArray().then((data)=>{sendInsult = data[0].insult})
+        interval = setInterval(()=>{waitForInsult(message)},250);
+
         listOfEmotes = Array.from(message.guild.emojis.cache);
-        
         //console.log(listOfEmotes);
         const reactionEmoji = listOfEmotes[Math.floor(Math.random() * listOfEmotes.length)];
         message.react(reactionEmoji[1].id);
         
     }
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot){
+        return;
+    } 
     const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
@@ -70,6 +98,7 @@ bot.on('message',(message)=>{
         bot.commands.get(command).execute(message, args);
     }
 })
+
 
 exports.listOfCommands = listOfCommands;
 //git rm -rf --cached .
